3b5fca5774f58e473519eacaa48757c39c9d7f54
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-04-25 21:40:10 -0500

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $.getJSON("/articles", function(data) {
-    for (let i = 0; i < data.lenght; i++ ) {
+    for (var i = 0; i < data.lenght; i++ ) {
         $("#articles").append("<p data-id='" + 
         data[i]._id + "'>" + 
         data[i].title + "<br />" + 
